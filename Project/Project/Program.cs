0dfee8dea8baaf1029cdc53ba40a27e12e7874fc
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-28 17:50:27 +0300

    Update classes

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Project;
+
+var students = Student.PopulateStudents();
+
+var numberOfStudents = students.Where(s => s.University.Equals("UTCN")).ToList().Count;
+Console.WriteLine($"The number of students from UTCN is {numberOfStudents}.");
+Console.WriteLine();
+
+var groups = students.GroupBy(s => s.CurrentYear);
+Console.WriteLine("Students grouped by current year:");
+foreach (var group in groups)
+{
+    Console.WriteLine(group.Key);
+
+    foreach (var student in group)
+    {
+        Console.WriteLine(student);
+    }
+}
+Console.WriteLine();
---
+++ Project/Project/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int CurrentYear { get; set; }
+
+        public static List<Student> PopulateStudents()
+        {
+            return new List<Student>
+            {
+                new Student { Id = 73, Name = "Tarah Symons", University = "UTCN", CurrentYear = 2 },
+                new Student { Id = 12, Name = "Gracie Jacobs", University = "UBB", CurrentYear = 2 },
+                new Student { Id = 34, Name = "Danny Barr", University = "UBB", CurrentYear = 3 },
+                new Student { Id = 56, Name = "Tylor Kirk", University = "UTCN", CurrentYear = 3 },
+                new Student { Id = 89, Name = "Ruby Moore", University = "UPB", CurrentYear = 1 },
+                new Student { Id = 92, Name = "Tracie Bryan", University = "UTCN", CurrentYear = 3 },
+                new Student { Id = 18, Name = "Kelvin Newport", University = "UTCN", CurrentYear = 1 }
+            };
+        }
+
+        public override string? ToString() => $"{Id} | {Name} | {University} | {CurrentYear}";
+    }
+}
